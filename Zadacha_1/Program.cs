53b5246197ca5777107967ffdea47d202c46eed1
«Dmitrii <bimas2006@mail.ru>
2022-04-20 16:52:14 +0300

    3 домашнее задание v2

--- Zadacha_1/Program.cs
+++ Zadacha_1/Program.cs
@@ -1,19 +1,19 @@
-﻿int a, b = 0, c= 0;
-        
-            Console.Write("Введите 5-и значное положительное число: ");
-            a = Int32.Parse(Console.ReadLine());
-            c = a;
-            while (a > 0) 
-            {
-                b = b * 10;
-                b = b + a % 10;
-                a = a /10;
-            }
-            if (c == b)
-            {
-                Console.Write($"Это палином ");
-            }
-            else
-            {
-                Console.Write($" Это не палином ");
-            }
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+int a, b = 0, c= 0;
+Console.Write("Введите 5-и значное положительное число: ");
+a = Int32.Parse(Console.ReadLine());
+c = a;
+while (a > 0) 
+{
+    b = b * 10;
+    b = b + a % 10;
+    a = a /10;
+}
+if (c == b)
+{
+    Console.Write($"Это палином ");
+}
+else
+{
+    Console.Write($" Это не палином ");
+}
--- Zadacha_2/Program.cs
+++ Zadacha_2/Program.cs
@@ -1,8 +1,9 @@
-﻿double xa = 34;
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+double xa = 34;
 double ya = 87;
-double za = 15;
-double xb = 48;
-double yb = 12;
-double zb = -79;
+double za = -15;
+double xb = 18;
+double yb = 125;
+double zb = 47;
 double A = Math.Sqrt((xb-xa)*(xb-xa)+(yb-ya)*(yb-ya)+(zb-za)*(zb-za));
 Console.Write($"Расстояние между точками А и B = {Math.Round(A, 2)}");
