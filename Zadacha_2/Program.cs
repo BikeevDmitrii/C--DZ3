9c02616430707f0818ebc27da4b19d7272c7673a
«Dmitrii <bimas2006@mail.ru>
2022-04-20 15:21:27 +0300

    3 домашняя работа

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,8 @@
+﻿double xa = 34;
+double ya = 87;
+double za = 15;
+double xb = 48;
+double yb = 12;
+double zb = -79;
+double A = Math.Sqrt((xb-xa)*(xb-xa)+(yb-ya)*(yb-ya)+(zb-za)*(zb-za));
+Console.Write($"Расстояние между точками А и B = {Math.Round(A, 2)}");
---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,8 @@
+﻿int N = 6, a = 0;
+Console.Write($"{N} -> ");
+while (a < N)
+{
+    Console.Write($"{a * a * a}, ");
+    a++;
+}
+Console.Write($"{a * a * a}.");
